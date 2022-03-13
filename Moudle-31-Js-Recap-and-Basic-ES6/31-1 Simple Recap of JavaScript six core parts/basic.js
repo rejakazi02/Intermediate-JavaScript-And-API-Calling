35d9a3e96d49f4a76bf2e87a91b6488f18c31339
Reja Kazi <rejakazi02@gmail.com>
2022-03-13 13:06:30 +0600

    JavaScript six core parts , array, loop, condition, function, object, variable

--- Moudle-31-Js-Recap-and-Basic-ES6/31-1 Simple Recap of JavaScript six core parts/basic.js
+++ Moudle-31-Js-Recap-and-Basic-ES6/31-1 Simple Recap of JavaScript six core parts/basic.js
@@ -1,1 +1,77 @@
+
+// variable
 var deposit = 400;
+// Condition
+
+if(deposit > 500){
+
+}
+
+
+if(deposit < 200){
+
+}
+
+
+if(deposit == 500){
+
+}
+
+
+if(deposit != 500){
+
+}
+
+
+if(deposit >= 500){
+
+}
+
+if(deposit <= 500){
+
+}
+ else {
+
+ }
+
+//  Array
+
+const number = [65, 66684, 124, 45, 1, 365];
+const numberConst = number.length;
+number.pop();
+number.push(111);
+number[2] = 555;
+
+// check whether 222 include in the array
+// i can do true false condition work using this .
+
+if (number.indexOf(2)) != -1 {
+    
+}
+if (number.includes(222)) {
+    
+}
+
+// loop
+// while loop, for
+
+
+for (const numbers of number) {
+    
+}
+
+//function
+
+function fullName(first, second) {
+    const name = first + '' + second;
+    }
+const person = fullName('Rahim', 'kaka');
+
+// Object
+
+const bottle = {
+
+    color = 'yellow',
+    price = 250,
+    
+}
