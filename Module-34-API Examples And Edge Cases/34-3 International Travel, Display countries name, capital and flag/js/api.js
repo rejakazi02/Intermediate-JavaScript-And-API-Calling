696d2576d6c69fdcc4b6d70447e120db351a73d5
Reja Kazi <rejakazi02@gmail.com>
2022-03-19 03:15:35 +0600

    Rest Country Api International Travel, Display countries name, capital and flag

--- Module-34-API Examples And Edge Cases/34-3 International Travel, Display countries name, capital and flag/js/api.js
+++ Module-34-API Examples And Edge Cases/34-3 International Travel, Display countries name, capital and flag/js/api.js
@@ -1,3 +1,8 @@
+
+
+
+
+
 const loadCountries = () => {
 
     fetch('https://restcountries.com/v3.1/all')
@@ -10,15 +15,15 @@ loadCountries();
 const displayData = countryData => {
 
 
-console.log(countryData)
+    console.log(countryData)
 
-const countryInfo = document.getElementById('country');
-countryData.forEach(country => {
+    const countryInfo = document.getElementById('country');
+    countryData.forEach(country => {
 
-    const countryDiv = document.createElement('div');
-    countryDiv.classList.add('mydivStyle');
-    console.log(country)
-    countryDiv.innerHTML = `
+        const countryDiv = document.createElement('div');
+        countryDiv.classList.add('mydivStyle');
+        console.log(country)
+        countryDiv.innerHTML = `
     
        <h1>Country Name : ${country.name.common}</h1> 
        <h5>Capital : ${country.capital}</h5>
@@ -29,33 +34,14 @@ countryData.forEach(country => {
        <img src="${country.flags.png} " alt="">
     `;
 
-    countryInfo.appendChild(countryDiv);
-    
-});
+        countryInfo.appendChild(countryDiv);
 
-    // const values = datas.results;
-
-    // const ranData = document.getElementById('randomUser');
-
-    // for (const value of values) {
-
-    //     const ptag = document.createElement('div');
-    //     console.log(value)
-    //     ptag.classList.add("myStyle");
-    //     ptag.innerHTML = ` 
- 
-    //      <h1> Name : ${value.name.title} ${value.name.first} ${value.name.last}</h1> 
-    //      <p>Gender : ${value.gender}</p>
-    //      <p>Phone : ${value.phone} </p>
-    //      <img src="${value.picture.large} " alt="">
-         
-         
-    //      `;
-
-    //     ranData.appendChild(ptag);
-
-
-    // }
+    });
 
 
 }
+
+
+
+
+
