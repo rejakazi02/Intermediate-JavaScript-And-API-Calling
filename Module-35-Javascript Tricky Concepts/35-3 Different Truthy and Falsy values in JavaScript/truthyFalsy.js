a175a4bdd6f91df2387fc65793adc64805023bf4
Reja Kazi <rejakazi02@gmail.com>
2022-03-26 13:10:32 +0600

    Different Truthy and Falsy values in JavaScript

---
+++ Module-35-Javascript Tricky Concepts/35-3 Different Truthy and Falsy values in JavaScript/truthyFalsy.js
@@ -0,0 +1,42 @@
+
+
+//   Different Truthy and Falsy values in JavaScript
+
+
+/*
+
+
+Falsy
+false
+0 
+empty String
+undefined
+null
+NaN.
+
+Truthy
+
+true 
+any number( positive or negative)
+any  string includeing single whitespace, '0' , 'false'
+[object]
+{object}
+anything else that is not falsy will be truthy
+
+*/
+
+
+
+let x = 2;
+
+if (x) {
+
+    console.log('this value is truthy');
+
+}
+
+else {
+
+    console.log('this value is falsy');
+}
+ 
