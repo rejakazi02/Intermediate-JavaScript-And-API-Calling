aa6342c539aae7b6e833436504a66524aa85fdad
Reja Kazi <rejakazi02@gmail.com>
2022-03-26 19:44:12 +0600

    Double equal (==) vs triple equal (===), implicit conversion

---
+++ Module-35-Javascript Tricky Concepts/35-5 Double equal (==) vs triple equal (===), implicit conversion/double-triple-Equal.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+const first = 1;
+const second = true;
+
+if (first == second) {
+    //  console.log('condition is true');
+
+} else {
+
+    //console.log('condition is false');
+}
+
+
+// more comparison 
+
+const a = {
+    name: 'ali'
+};
+const b = {
+    name: 'ali'
+};
+// const a = 1; 
+if (a.name === b.name) {
+    console.log('Both are same');
+} else {
+
+    console.log('Both are not same');
+}
+
+
+
+
