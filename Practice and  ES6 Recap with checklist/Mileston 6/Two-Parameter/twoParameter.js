016623777f54b43d80fb89698c590120aad09935
Reja Kazi <rejakazi02@gmail.com>
2022-03-31 02:21:10 +0600

    Two Parameter Add

---
+++ Practice and  ES6 Recap with checklist/Mileston 6/Two-Parameter/twoParameter.js
@@ -0,0 +1,19 @@
+
+
+
+
+const twoParameter = (num1, num2) => {
+    const sum = num1 + 7;
+    const sum1 = num2 + 7;
+    const value = sum + sum1;
+    return value;
+    
+}
+
+console.log(twoParameter(17, 17));
+
+
+
+
+
+
