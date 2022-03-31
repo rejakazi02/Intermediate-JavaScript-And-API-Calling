87a7bffc747388c80fe73f6a54ea8bff37f06a1c
Reja Kazi <rejakazi02@gmail.com>
2022-03-31 13:05:55 +0600

    Destructuring

---
+++ Practice and  ES6 Recap with checklist/Mileston 6/Destructuring/desArray.js
@@ -0,0 +1,34 @@
+
+
+
+const mobile = {
+    name: 'Xiaomi',
+    color: "black",
+    weight: "178.8 grams",
+    Performance: {
+      pro: "octa core",
+      RAM: "4 GB",
+      OS: "Android 11 (MIUI 12)",
+      price: 50000,
+      Sensors: ["Accelerometer", "Fingerprint", "Gyroscope", "Proximity"],
+    },
+  };
+
+  
+  const {name, color, weight} = mobile;
+  const [Accelerometer, balance, Gyroscope, Proximity] = mobile.Performance.Sensors;
+
+  console.log(name, balance);
+
+
+
+
+
+
+
+
+
+
+
+
+
