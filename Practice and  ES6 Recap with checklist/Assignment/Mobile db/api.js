4e0777b75ba6959601984ed01c56601411e7857d
Reja Kazi <rejakazi02@gmail.com>
2022-04-03 18:04:20 +0600

    Try case and Successfull messege print

--- Practice and  ES6 Recap with checklist/Assignment/Mobile db/api.js
+++ Practice and  ES6 Recap with checklist/Assignment/Mobile db/api.js
@@ -1,21 +1,26 @@
 const searchButton = () => {
 
 
-    const productDetailDiv = document.getElementById("productResult").style.display = "none";
+  const productDetailDiv = document.getElementById("productResult").style.display = "none";
 
 
   const searchField = document.getElementById("search-field");
- const searchText = searchField.value;
- 
+  const searchText = searchField.value;
+
   const emptyMesseges = document.getElementById("empty-Messege");
+ emptyMesseges.style.display = "block";
 
   searchField.value = "";
- 
+
+// saerch button condition
+
   if (searchText == "") {
     emptyMesseges.innerText = "Please Search your Products.";
-  } else {
+  } 
+  
+  else {
     emptyMesseges.innerText = " Search Successfull";
-   const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
+    const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`
     fetch(url)
       .then((res) => res.json())
       .then((datas) => displayData(datas.data))
@@ -26,71 +31,109 @@ const searchButton = () => {
 
 const displayData = (datas) => {
   const searchResult = document.getElementById("search-result");
-  searchResult.innerHTML = ''; 
-searchResult.value = '';
+  searchResult.innerHTML = '';
+  searchResult.value = '';
   console.log(datas)
 
-  datas.forEach(data => {
+
+// try case start here 
+
+  try {
+
+
+    datas.forEach((data, inde) => {
+
+// index check and break 
+
+      console.log(inde)
+      if (inde === 3) {
+        throw 'Break';
+  
+      }
+     
+      const div = document.createElement("div");
+      div.classList.add("col");
+      div.innerHTML = `
+      <div class="container-fluid card card-color">
+      <div class="d-flext">
+          <div class="d-flext justify-content-center">
+              <img src="${data.image}" class="image-custom card-img-top mt-2" alt="..." />
+          </div>
+      </div>
+  
+      <div class="card-body">
+          <h4 class="card-title text-center">${data.phone_name}</h4>
+          <hr />
+          <h5 class="text-center text-secondary">
+              Brand name:<span class="px-2 text-primary">${data.brand}</span>
+          </h5>
+          <hr />
+  
+  <div id="detaileBtn">
+  
+  <button onclick="detailsButton('${data.slug}')"
+  class="btn btn-outline-success px-5" id="detailsButton12">
+  Details
+  </button>
+  </div>
+      </div>
+  </div>
+  
+  `;
+  
+      searchResult.appendChild(div);
+
+     
+  
+    })
+
+
+  }
+  
+  // catch Pooerty 
+
+  catch (e) {
+    if (e !== 'Break') throw e
+  }
+
+  
  
-    const div = document.createElement("div");
-    div.classList.add("col");
-    div.innerHTML = `
-    <div class="container-fluid card card-color">
-    <div class="d-flext">
-        <div class="d-flext justify-content-center">
-            <img src="${data.image}" class="image-custom card-img-top mt-2" alt="..." />
-        </div>
-    </div>
-
-    <div class="card-body">
-        <h4 class="card-title text-center">${data.phone_name}</h4>
-        <hr />
-        <h5 class="text-center text-secondary">
-            Brand name:<span class="px-2 text-primary">${data.brand}</span>
-        </h5>
-        <hr />
-
-<div id="detaileBtn">
-
-<button onclick="detailsButton('${data.slug}', '${data.image}')"
-class="btn btn-outline-success px-5" id="detailsButton12">
-Details
-</button>
-</div>
-    </div>
-</div>
-
-`;
-
-    searchResult.appendChild(div);
-  });
 }
 
 
 
 const detailsButton = (pSlug) => {
-    const productDetailDiv = document.getElementById("productResult").style.display = "block";
-    console.log(pSlug)
-const url = `https://openapi.programming-hero.com/api/phone/${pSlug}`;
-fetch(url)
-.then(rer => rer.json())
-.then(dDatas => Pdetails(dDatas.data))
+  const productDetailDiv = document.getElementById("productResult").style.display = "block";
+  const emptyMesseges = document.getElementById("empty-Messege").style.display = "none";
+  console.log(pSlug)
+  const url = `https://openapi.programming-hero.com/api/phone/${pSlug}`;
+  fetch(url)
+    .then(rer => rer.json())
+    .then(dDatas => Pdetails(dDatas.data))
 
 
 }
 
 
 const Pdetails = (proDatas) => {
-const productResults = document.getElementById('productResult');
+  const productResults = document.getElementById('productResult');
+
+  productResults.innerHTML = '';
+
+  const proDiv = document.createElement('div');
+  proDiv.classList.add('card');
+
+// release date null condition 
+
+  let relesrDate = proDatas.releaseDate;
+  if (relesrDate == '') {
+
+    relesrDate = "No date found";
 
 
-productResults.innerHTML = '';
+  }
 
-const proDiv = document.createElement('div');
-proDiv.classList.add('card');
-
-
-proDiv.innerHTML = `
+  proDiv.innerHTML = `
 
 <div class="d-flex container-fluid">
 <div class="imgCustom col-5">
@@ -108,6 +151,7 @@ proDiv.innerHTML = `
   <small class="text-secondary">WLAN: ${proDatas.others?.WLAN}</small>
   <br>
   <small class="text-secondary">Bluetooth: ${proDatas.others?.Bluetooth}</small>
+
   <br>
   <small class="text-secondary">GPS: ${proDatas.others?.GPS}</small>
   <br>
@@ -122,12 +166,12 @@ proDiv.innerHTML = `
 <hr>
       <div class="card-body">
         <h4 class="card-title text-center  text-primary">${proDatas.name}</h4>
-        <small class="text-center mt-2">Release Date:  <span class="text-secondary">${proDatas.releaseDate}</span></small>
+        <small class="text-center mt-2">Release Date:  <span id="dateee1" class="text-secondary"> ${relesrDate}</span></small>
       </div>
 
       `;
 
 
-productResults.appendChild(proDiv);
+  productResults.appendChild(proDiv);
 
 }
