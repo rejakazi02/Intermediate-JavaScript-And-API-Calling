bc52b6af432b137db97215f1dc4d054d68d87e14
Reja Kazi <rejakazi02@gmail.com>
2022-04-04 22:10:14 +0600

    weacher chart

---
+++ Module-37.5-Bonsu Api and Js Recap/37_5-1 Get API key and load temperature data by city name/js/api.js
@@ -0,0 +1,42 @@
+const API_KEY = `e909cfb76a1aa66ca5139ed4fd8417dd`;
+
+const searchBtn = () => {
+ const searchFiled = document.getElementById('searchFiled').value;
+
+
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchFiled}&appid=${API_KEY}&units=metric`)
+.then(res => res.json())
+.then(data => displayDetails(data))
+
+}
+
+
+const displayDetails = datas => {
+
+
+    const imgg = document.getElementById("imgg");
+   imgg.innerHTML = '';
+
+    const ing = document.createElement('div');
+    ing.classList.add("wediv");
+
+
+    ing.innerHTML = `<img src=" http://openweathermap.org/img/wn/${datas.weather[0].icon}@2x.png" class="imgg image-custom card-img-top mt-2" alt="..."  />
+    
+    <h1 id="city">${datas.name}</h1>
+            <h3><span id="temp">${datas.main.temp}</span>&deg;C</h3>
+            <h1 id="weather" class="lead">${datas.weather[0].main}</h1>
+    
+    `;
+ imgg.appendChild(ing);
+
+
+//  const city = document.getElementById("city");
+//  const temp = document.getElementById("temp");
+//  const weather = document.getElementById("weather");
+
+//  city.innerHTML = `${datas.name}`;
+//  temp.innerHTML = `${datas.main.temp}`;
+//  weather.innerHTML = `${datas.weather[0].main}`;
+    console.log(datas)
+}
