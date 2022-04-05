b3418737e140c066ae94ff4c71048192a88c5073
Reja Kazi <rejakazi02@gmail.com>
2022-04-06 04:49:06 +0600

    Browser alert, confirm, prompt with examples

---
+++ Module-39-Browser API and methods/39-1 Browser alert, confirm, prompt with examples/alart.js
@@ -0,0 +1,28 @@
+const maComing = () => {
+
+    alert('Ma is coming');
+}
+
+
+const askPicnic = () => {
+
+    const responce = confirm('Are you going Picnic ?');
+    if (responce === true) {
+        alert('Amake bkash kor');
+    } else {
+        alert('muri kha');
+    }
+
+}
+
+
+const askName = () => {
+    const name = prompt("What's your name?");
+
+    if (name) {
+        console.log(name);
+    }
+
+}
+
+
